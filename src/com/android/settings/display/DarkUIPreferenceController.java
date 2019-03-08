AOSiP/platform_packages_apps_Settings

Settings: Disable system theme preferences when Substratum overlays are enabled
Rather than unconditionally disabling system themes when Substratum is detected,
only do so when there is a Substratum theme applied to the framework.

Improves on a (not so) similar commit by Alex Cruz.

Change-Id: I8c404eee30f4070466fbca5232acd5c225c622eb
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

src/com/android/settings/display/DarkUIPreferenceController.java
@@ -22,6 +22,8 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 
+import static com.android.settings.display.ThemeUtils.isSubstratumOverlayInstalled;
+
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
@@ -59,12 +61,17 @@ public class DarkUIPreferenceController extends AbstractPreferenceController imp
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mSystemUiThemeStyle = (ListPreference) screen.findPreference(SYSTEM_UI_THEME);
-        int systemuiThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SYSTEM_UI_THEME, 0);
-        int valueIndex = mSystemUiThemeStyle.findIndexOfValue(String.valueOf(systemuiThemeStyle));
-        mSystemUiThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntry());
-        mSystemUiThemeStyle.setOnPreferenceChangeListener(this);
+        if (!isSubstratumOverlayInstalled(mContext)) {
+            int systemuiThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SYSTEM_UI_THEME, 0);
+            int valueIndex = mSystemUiThemeStyle.findIndexOfValue(String.valueOf(systemuiThemeStyle));
+            mSystemUiThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+            mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntry());
+            mSystemUiThemeStyle.setOnPreferenceChangeListener(this);
+        } else {
+            mSystemUiThemeStyle.setEnabled(false);
+            mSystemUiThemeStyle.setSummary(R.string.substratum_installed_title);
+        }
     }
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {

IPSBHANGU/platform_packages_apps_Settings-2

Change-Id: If1b601a7184c96fc33347da2cfff5ac2f2d7aca7

src/com/android/settings/display/DarkUIPreferenceController.java
@@ -22,6 +22,8 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 
+import static com.android.settings.display.ThemeUtils.isSubstratumOverlayInstalled;
+
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
@@ -59,12 +61,17 @@ public class DarkUIPreferenceController extends AbstractPreferenceController imp
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mSystemThemeStyle = (ListPreference) screen.findPreference(SYSTEM_THEME_STYLE);
-        int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SYSTEM_THEME_STYLE, 0);
-        int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
-        mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
-        mSystemThemeStyle.setOnPreferenceChangeListener(this);
+        if (!isSubstratumOverlayInstalled(mContext)) {
+            int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SYSTEM_THEME_STYLE, 0);
+            int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
+            mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+            mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
+            mSystemThemeStyle.setOnPreferenceChangeListener(this);
+        } else {
+            mSystemThemeStyle.setEnabled(false);
+            mSystemThemeStyle.setSummary(R.string.substratum_installed_title);
+        }
     }
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {