SuperiorOS/android_packages_apps_Settings

Add version and build date to Settings
  Adapted for P by @ishubhamsingh

 * Use seperate prefController for both
 * Based on implementation of FirmwareVersion
 * Add Version to Firmware Version Dialogue

Signed-off-by: Shubham Singh <coolsks94@gmail.com>

src/com/android/settings/deviceinfo/DeviceInfoSettings.java
@@ -31,6 +31,8 @@ import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
+import com.android.settings.deviceinfo.BuildDatePreferenceController;
+import com.android.settings.deviceinfo.SuperiorVersionPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -126,6 +128,8 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new PhoneNumberPreferenceController(context));
         controllers.add(new SimStatusPreferenceController(context, fragment));
+        controllers.add(new SuperiorVersionPreferenceController(context, lifecycle));
+        controllers.add(new BuildDatePreferenceController(context));
         controllers.add(new DeviceModelPreferenceController(context, fragment));
         controllers.add(new ImeiInfoPreferenceController(context, fragment));
         controllers.add(new FirmwareVersionPreferenceController(context, fragment));

src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -48,6 +48,8 @@ import com.android.settings.deviceinfo.RegulatoryInfoPreferenceController;
 import com.android.settings.deviceinfo.SafetyInfoPreferenceController;
 import com.android.settings.deviceinfo.WifiMacAddressPreferenceController;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionPreferenceController;
+import com.android.settings.deviceinfo.BuildDatePreferenceController;
+import com.android.settings.deviceinfo.SuperiorVersionPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -119,6 +121,8 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new DeviceModelPreferenceController(context, fragment));
         controllers.add(new ImeiInfoPreferenceController(context, fragment));
         controllers.add(new FirmwareVersionPreferenceController(context, fragment));
+        controllers.add(new SuperiorVersionPreferenceController(context, lifecycle));
+        controllers.add(new BuildDatePreferenceController(context));
         controllers.add(new IpAddressPreferenceController(context, lifecycle));
         controllers.add(new WifiMacAddressPreferenceController(context, lifecycle));
         controllers.add(new BluetoothAddressPreferenceController(context, lifecycle));

src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDialogFragment.java
@@ -85,6 +85,7 @@ public class FirmwareVersionDialogFragment extends InstrumentedDialogFragment {
 
     private void initializeControllers() {
         new FirmwareVersionDialogController(this).initialize();
+        new SuperiorVersionDialogController(this).initialize();
         new SecurityPatchLevelDialogController(this).initialize();
         new BasebandVersionDialogController(this).initialize();
         new KernelVersionDialogController(this).initialize();

IPSBHANGU/platform_packages_apps_Settings-2

src/com/android/settings/deviceinfo/DeviceInfoSettings.java
@@ -31,6 +31,8 @@ import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
+import com.android.settings.deviceinfo.BuildDatePreferenceController;
+import com.android.settings.deviceinfo.AospVersionPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -126,6 +128,8 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new PhoneNumberPreferenceController(context));
         controllers.add(new SimStatusPreferenceController(context, fragment));
+        controllers.add(new AospVersionPreferenceController(context, lifecycle));
+        controllers.add(new BuildDatePreferenceController(context));
         controllers.add(new DeviceModelPreferenceController(context, fragment));
         controllers.add(new ImeiInfoPreferenceController(context, fragment));
         controllers.add(new FirmwareVersionPreferenceController(context, fragment));

src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -49,6 +49,8 @@ import com.android.settings.deviceinfo.SafetyInfoPreferenceController;
 import com.android.settings.deviceinfo.SELinuxStatusPreferenceController;
 import com.android.settings.deviceinfo.WifiMacAddressPreferenceController;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionPreferenceController;
+import com.android.settings.deviceinfo.BuildDatePreferenceController;
+import com.android.settings.deviceinfo.AospVersionPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -120,6 +122,8 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new DeviceModelPreferenceController(context, fragment));
         controllers.add(new ImeiInfoPreferenceController(context, fragment));
         controllers.add(new FirmwareVersionPreferenceController(context, fragment));
+        controllers.add(new AospVersionPreferenceController(context, lifecycle));
+        controllers.add(new BuildDatePreferenceController(context));
         controllers.add(new IpAddressPreferenceController(context, lifecycle));
         controllers.add(new WifiMacAddressPreferenceController(context, lifecycle));
         controllers.add(new BluetoothAddressPreferenceController(context, lifecycle));

src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDialogFragment.java
@@ -85,6 +85,7 @@ public class FirmwareVersionDialogFragment extends InstrumentedDialogFragment {
 
     private void initializeControllers() {
         new FirmwareVersionDialogController(this).initialize();
+        new AospVersionDialogController(this).initialize();
         new SecurityPatchLevelDialogController(this).initialize();
         new BasebandVersionDialogController(this).initialize();
         new KernelVersionDialogController(this).initialize();